bc21f825abb99a66088bc044d2b87cb07929f73a
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-02-24 22:02:30 -0800

    with classNames, if statement with number of spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,24 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
+let formatSpots = function(spots) {
+  if (spots === 0) {
+    return "no spots remaining";
+  } else if (spots === 1) {
+    return "1 spot remaining";
+  } else {
+    return`${spots} spots remaining`;
+  }
+}
 
 export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots == 0});
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
