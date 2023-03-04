c6362c8ce50d25d4f05095237246be43e6ece26c
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-03-03 21:40:05 -0800

    with axios and making request to scheduler-api for data

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = {
   "1": {
@@ -62,8 +63,16 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDay] = useState([]);
 
+  useEffect(() => {
+    const getRequest = `http://localhost:8001/api/days`
+    axios.get(getRequest).then((response) => {
+      setDay(response.data)
+      console.log(response.data);  
+    });
+  },[]);
+  
   
   return (
     <main className="layout">
@@ -77,7 +86,7 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            value={day}
+            value={days}
             onChange={setDay}
           />
         </nav>
@@ -93,6 +102,7 @@ export default function Application(props) {
         })}
         <Appointment key="last" time ="5pm"></Appointment>
       </section>
+      
     </main>
   );
 }
