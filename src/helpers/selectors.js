d8018ee0adf22f8d85b5b3a9f2d6383cbe11a39a
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-03-06 21:36:45 -0800

    hooks with mode, history and back in useVisualMode

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,7 @@
 export function getAppointmentsForDay(state, day) {
   const dayAppointment = state.days.find((days) => days.name === day);
-  console.log(dayAppointment)
 
-  const appointmentsArray = dayAppointment ? dayAppointment.appointments.map(id => state.appointments[id]) : []
+  const appointmentsArray = dayAppointment ? dayAppointment.appointments.map(id => state.appointments[id]) : [];
 
   return appointmentsArray;
 };
---
+++ src/hooks/useVisualMode.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false)  {
+    setMode(newMode);
+    if (replace) {
+      setHistory(prev => [...prev.slice(prev.length - 2, prev.length - 1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode])
+    } 
+  }
+
+  function back() {
+    if (history.length > 1 && history.pop()) {
+      setMode(history[history.length - 1])
+    }
+  }
+  
+  return { mode, transition, back };
+
+}
+
